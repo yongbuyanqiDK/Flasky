f2036f13bd18fa5cdfb59bbd118daa0dabb8bc23
董凯 <179367003@qq.com>
2015-11-19 16:28:53 +0800

    add test

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,2 +1,23 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'inso'
+import unittest
+from flask import current_app
+from app import create_app,db
+
+class BasicsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app=create_app('testing')
+        self.app_context=self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
+
+    def test_app_is_testing(self):
+        self.assertTrue(current_app.config['TESTING'])
